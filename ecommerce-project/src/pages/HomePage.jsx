fcfd9ca9570fc6a26e202293e5040a92f51ec657
Anjan012 <anjankhadka199@gmail.com>
2025-11-23 11:39:03 +0545

    use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,13 @@
+import axios from "axios";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
 
-    fetch('http://localhost:3000/api/products') // this will fetch the data from backend and in that time the code will not wait keeps executing and in the future when the data is received it will execute the then block which will execute the function inside it. when fetch gets the data it will save the data in the parameter below response
+    axios.get('http://localhost:3000/api/products') // this will fetch the data from backend and in that time the code will not wait keeps executing and in the future when the data is received it will execute the then block which will execute the function inside it. when fetch gets the data it will save the data in the parameter below response note: enlike fetch we will get the data in the response.data and it is a cleaner way to make API requests
         .then((response) => {
-            // console.log(response);
-            return response.json()// .json gives us the data attached to the response response.json() is also asynchronous so we cannot save it in a variabe
-        }).then((data) => {
-            console.log(data);
+            console.log(response);
         }); 
 
     return (
