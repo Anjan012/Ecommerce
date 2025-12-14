8562eb28bc2ef5e3d895fe8ecf85edd4ec523a99
Anjan012 <anjankhadka199@gmail.com>
2025-12-14 12:07:34 +0545

    Test Clicking add to cart button

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,6 +1,10 @@
 import { it, expect, describe, vi } from "vitest";
 import { render, screen } from "@testing-library/react"; // this renders a component in a fake web page, screen lets us check the fake web page
+import userEvent from "@testing-library/user-event"; // this lets us simulate event like clicking a button
+import axios from "axios";
 import { Product } from "./Product";
+
+vi.mock('axios'); // this mocks the entire axios package when we import from axios we will get a fake version of axios
 
 describe('Product component', () => {
     it('displays the products details correctly', () => {
@@ -43,4 +47,36 @@ describe('Product component', () => {
             screen.getByText('87')
         ).toBeInTheDocument();
     });
+
+    it('adds a product to a cart', async () => {
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
+
+        const loadCart = vi.fn(); 
+        render(<Product product={product} loadCart={loadCart}/>);
+
+        const user = userEvent.setup(); // setup event now we can simulate event
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        await user.click(addToCartButton); // since clicking a button requires time we need to use await 
+
+        // This checks when we click add to cart does our code run axios.post and give it these values even though mock doesn't do anything we can still check does we call and gave them correct values
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 1
+            }
+        );
+        expect(loadCart).toHaveBeenCalled(); // it checks if it has been call only
+    })
+
 });
