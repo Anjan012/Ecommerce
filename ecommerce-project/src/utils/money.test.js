ba38b2d13e422a8b9567ba08c35f7c847c812c31
Anjan012 <anjankhadka199@gmail.com>
2025-12-14 08:47:27 +0545

    Create unit test for formatMoney

---
+++ ecommerce-project/src/utils/money.test.js
@@ -0,0 +1,17 @@
+import { it, expect, describe } from "vitest";
+import { formatMoney } from "./money";
+
+describe("formatMoney", () => {
+  it("formats 1999 cents as 19.99", () => {
+    expect(formatMoney(1999)).toBe("$19.99");
+  }); // it creates the test and the string is the name of the test
+
+  it("displays 2 decimals", () => {
+    expect(formatMoney(1090)).toBe("$10.90");
+    // adding another checks
+    expect(formatMoney(100)).toBe("$1.00");
+  });
+});
+
+// note: all of the checks needs to pass inorder for this test to pass if one fails the entire test fails
+// describe: groups test together we call it testsuite a best practice is to puts test in a testsuite even we have only one test  
