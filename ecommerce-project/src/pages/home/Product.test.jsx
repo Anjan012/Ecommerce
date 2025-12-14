f5ae22b471bdd5dbf27db889821a5cc8684aa6c0
Anjan012 <anjankhadka199@gmail.com>
2025-12-14 12:14:52 +0545

    Use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react"; // this renders a component in a fake web page, screen lets us check the fake web page
 import userEvent from "@testing-library/user-event"; // this lets us simulate event like clicking a button
 import axios from "axios";
@@ -7,10 +7,16 @@ import { Product } from "./Product";
 vi.mock('axios'); // this mocks the entire axios package when we import from axios we will get a fake version of axios
 
 describe('Product component', () => {
-    it('displays the products details correctly', () => {
-        // While testing the component we render the component
 
-        const product = {
+    let product;
+
+    // we should not contact a real backend cause the backend might not be available during the test and it might accidently update the real data a best practice is to mock the function in our test {mock : create a fake version of the function  }
+    let loadCart; 
+
+
+    // befoore each test we can recreate this variables
+    beforeEach(() => { // beforeEach is a test hook
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -20,11 +26,14 @@ describe('Product component', () => {
             },
             priceCents: 1090,
             keywords: ["socks", "sports", "apparel"]
-        };
+        }
 
-        // we should not contact a real backend cause the backend might not be available during the test and it might accidently update the real data a best practice is to mock the function in our test {mock : create a fake version of the function  }
-        const loadCart = vi.fn(); // vi.fn() : creates a fake function that doesn't do anything 
-        render(<Product product={product} loadCart={loadCart}/>);
+        loadCart = vi.fn();
+    })
+
+    it('displays the products details correctly', () => {
+
+        render(<Product product={product} loadCart={loadCart} />);
 
 
         expect(
@@ -49,20 +58,8 @@ describe('Product component', () => {
     });
 
     it('adds a product to a cart', async () => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        };
 
-        const loadCart = vi.fn(); 
-        render(<Product product={product} loadCart={loadCart}/>);
+        render(<Product product={product} loadCart={loadCart} />);
 
         const user = userEvent.setup(); // setup event now we can simulate event
         const addToCartButton = screen.getByTestId('add-to-cart-button');
